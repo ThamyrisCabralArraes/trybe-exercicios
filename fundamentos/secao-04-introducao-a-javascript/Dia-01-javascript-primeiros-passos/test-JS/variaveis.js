12a86fccf691006822b8b563ea7e6e0aa9f3327c
Thamyris <thamyriscabral@hotmail.com>
2022-11-23 09:37:04 -0300

    exemplos

---
+++ fundamentos/secao-04-introducao-a-javascript/Dia-01-javascript-primeiros-passos/test-JS/variaveis.js
@@ -0,0 +1,21 @@
+const myName = 'Thamyris';
+const birthCity = 'São Joaquim do Monte';
+
+let birthYear = 1988;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030;
+console.log(birthYear);
+
+const base = 5;
+const heigth = 8;
+const area = base * heigth;
+
+console.log(area);
+
+const perimeter = base * 2 + heigth * 2;
+
+console.log(perimeter);
