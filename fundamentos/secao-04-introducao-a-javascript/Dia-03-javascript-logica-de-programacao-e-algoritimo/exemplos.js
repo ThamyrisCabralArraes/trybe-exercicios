0792f1f68d5ba0a7dec34081138f7dceff4d3af1
Thamyris <thamyriscabral@hotmail.com>
2022-11-29 10:25:14 -0300

    Armazenando alguns exepmplos

---
+++ fundamentos/secao-04-introducao-a-javascript/Dia-03-javascript-logica-de-programacao-e-algoritimo/exemplos.js
@@ -0,0 +1,35 @@
+// let fruits = [3, 4, 10, 1, 12];
+// let result = 0;
+// for (let index = 0; index < fruits.length; index += 1) {
+//   result += fruits[index];
+// }
+// if (result > 15) {
+//   console.log(result);
+// } else {
+//   console.log(`Valor menor que 16`);
+// }
+
+// const n = 9;
+
+// let resultado = 100;
+// for (let index = 0; index <= n; index += 1) {
+//   resultado -= index;
+// }
+// console.log(resultado);
+
+// let a = 5;
+// console.log(!(a < 7));
+
+// let number = 100;
+// let result = 0;
+
+// for (let index = 0; index <= 100; index += 1) {
+//   result += index;
+// }
+// console.log(result);
+
+let result = 0;
+for (let index = 0; index < 100; index += 5) {
+  result++;
+}
+console.log(result);
