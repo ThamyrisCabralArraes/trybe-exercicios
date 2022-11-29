6848dc394c0bd8ae29afb00ffa08d1fbae1cff94
Thamyris <thamyriscabral@hotmail.com>
2022-11-29 12:10:07 -0300

    Final do exercicio

--- fundamentos/secao-04-introducao-a-javascript/Dia-03-javascript-logica-de-programacao-e-algoritimo/exercicios.js
+++ fundamentos/secao-04-introducao-a-javascript/Dia-03-javascript-logica-de-programacao-e-algoritimo/exercicios.js
@@ -42,3 +42,19 @@ for (let index = 0; index < array.length; index += 1) {
 
 console.log(biggestWord);
 console.log(smallestWord);
+
+let biggestPrimeNumber = 0;
+
+for (let currentNumber = 2; currentNumber <= 50; currentNumber += 1) {
+  let isPrime = true;
+  for (let currentDivisor = 2; currentDivisor < currentNumber; currentDivisor += 1) {
+    if (currentNumber % currentDivisor === 0) {
+      isPrime = false;
+    }
+  }
+  if (isPrime) {
+    biggestPrimeNumber = currentNumber;
+  }
+}
+
+console.log(biggestPrimeNumber);
