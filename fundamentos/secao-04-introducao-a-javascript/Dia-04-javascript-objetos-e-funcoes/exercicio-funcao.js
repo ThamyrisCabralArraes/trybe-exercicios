0a743ac3cc045161877c79c9a31a96b82442a748
Thamyris <thamyriscabral@hotmail.com>
2022-11-30 18:58:24 -0300

    Exercicios de function, continuacao

--- fundamentos/secao-04-introducao-a-javascript/Dia-04-javascript-objetos-e-funcoes/exercicio-funcao.js
+++ fundamentos/secao-04-introducao-a-javascript/Dia-04-javascript-objetos-e-funcoes/exercicio-funcao.js
@@ -7,6 +7,7 @@ function verificaPalindrome(valor) {
   }
 }
 console.log(verificaPalindrome('ama'));
+//
 
 let arr = [2, 3, 6, 7, 10, 1];
 let maior = 0;
@@ -19,3 +20,30 @@ function maiorValor() {
 }
 maiorValor(arr);
 console.log(maior);
+//
+
+let arr2 = [2, 4, 6, 7, 10, 0, -3];
+let menor = arr2.length;
+function menorNumero() {
+  for (let index = 0; index < arr2.length; index += 1) {
+    if (menor > arr2[index]) {
+      menor = index;
+    }
+  }
+}
+menorNumero(arr2);
+console.log(menor);
+//
+
+let arrNomes = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
+let maiorNome = arrNomes[0];
+
+function arrNome() {
+  for (let index = 0; index < arrNomes.length; index = +1) {
+    if (maiorNome < arrNomes[index].length) {
+      maiorNome = arrNomes[index];
+    }
+  }
+}
+arrNome(arrNomes);
+console.log(maiorNome);
