705b732e704de99e4ea53065f214a89718e5fb82
Thamyris <thamyriscabral@hotmail.com>
2022-12-01 10:32:21 -0300

    'Acrescentando mais exercicios'

--- fundamentos/secao-04-introducao-a-javascript/Dia-05-javascript-let-const-arrow-functions-e-template-literals/exercicio-01.js
+++ fundamentos/secao-04-introducao-a-javascript/Dia-05-javascript-let-const-arrow-functions-e-template-literals/exercicio-01.js
@@ -12,9 +12,14 @@ const pessoa = {
   nome: 'Henri',
   idade: 20,
 };
-pessoa = {
-  nome: 'Luna',
-  idade: 19,
-}; // Altere essa estrutura para corrigir o erro.
+pessoa.nome = 'Luna';
+pessoa.idade = 19;
+
 console.log('Nome:', pessoa.nome);
 console.log('Idade:', pessoa.idade);
+
+//
+
+let favoriteFood = 'Lasanha';
+favoriteFood = 'Hamburguer';
+console.log(favoriteFood);
