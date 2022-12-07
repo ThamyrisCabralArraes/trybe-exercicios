cdb74c85cf2c89f63657db989b8d3db40ffe6c5a
Thamyris <thamyriscabral@hotmail.com>
2022-12-07 11:34:10 -0300

    exemplos da trybe, resolvido os exemplos

---
+++ fundamentos/Seção-05-JavaScript-DOM-Eventos-e-Web Storage/Dia-01-JavaScript-DOM-e-seletores/scriptExemplo.js
@@ -0,0 +1,38 @@
+const administradorDeTempo = document.getElementById('header-container');
+administradorDeTempo.style.backgroundColor = 'green';
+const urgenteImportante = document.getElementsByClassName('emergency-tasks');
+const noUrgenteImportante = document.getElementsByClassName('no-emergency-tasks');
+const footer = document.getElementById('footer-container');
+
+const h3 = document.getElementsByTagName('h3');
+urgenteImportante[0].style.backgroundColor = 'pink';
+h3[0].style.backgroundColor = 'green';
+h3[1].style.backgroundColor = 'green';
+
+noUrgenteImportante[0].style.backgroundColor = 'yellow';
+h3[2].style.backgroundColor = 'black';
+h3[3].style.backgroundColor = 'black';
+
+footer.style.backgroundColor = 'darkgreen';
+
+// const header = document.getElementById('header-container');
+// header.style.backgroundColor = 'rgb(0, 176, 105)';
+
+// const emergencyTasks = document.getElementsByClassName('emergency-tasks')[0];
+// emergencyTasks.style.backgroundColor = 'rgb(255, 159, 132)';
+
+// const emergencyTasksHeaders = document.querySelectorAll('.emergency-tasks h3');
+// for (let index = 0; index < emergencyTasksHeaders.length; index += 1) {
+//   emergencyTasksHeaders[index].style.backgroundColor = 'rgb(165, 0, 243)';
+// }
+
+// const noEmergencyTasks = document.querySelector('.no-emergency-tasks');
+// noEmergencyTasks.style.backgroundColor = 'rgb(249, 219, 94)';
+
+// const noEmergencyTasksHeaders = document.querySelectorAll('.no-emergency-tasks h3');
+// for (let index = 0; index < noEmergencyTasksHeaders.length; index += 1) {
+//   noEmergencyTasksHeaders[index].style.backgroundColor = 'rgb(35, 37, 37)';
+// }
+
+// const footer = document.getElementById('footer-container');
+// footer.style.backgroundColor = 'rgb(0, 53, 51)';
