a25b03bf9035c21c7756cfca230596b1c481f608
thamyris <thamyriscabral@hotmail.com>
2023-01-05 10:34:29 -0300

    exercicio HOF

---
+++ fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-04-spread-operator-rest-parameters-obj-destructuring/exemplo1.js
@@ -0,0 +1,72 @@
+const items = ['Camiseta', 'Carregador', 'Chinelo'];
+
+const newItems = [...items, 'Meia'];
+
+console.log(newItems); // ['Camiseta', 'Carregador', 'Chinelo', 'Meia'];
+
+console.log('...');
+
+const spring = ['OUT', 'NOV', 'DEZ'];
+const summer = ['JAN', 'FEV', 'MAR'];
+const fall = ['ABR', 'MAI', 'JUN'];
+const winter = ['JUL', 'AGO', 'SET'];
+
+const months = [...summer, ...fall, ...winter, ...spring];
+console.log(months);
+
+console.log('...');
+
+const product = {
+  id: 1,
+  name: 'Camiseta',
+};
+
+// adiciona a chave `price` com o valor `23`
+const newProduct = { ...product, price: 23 };
+
+console.log(newProduct); // {id: 1, name: 'Camiseta', price: '23'}
+
+console.log('...');
+
+const product2 = {
+  id: 1,
+  name: 'Camiseta',
+};
+
+const productPrice = {
+  price: 23,
+};
+
+const newProduct2 = { ...product2, ...productPrice };
+
+console.log(newProduct2);
+
+console.log('...');
+
+const randomNumbers = [57, 8, 5, 800, 152, 74, 630, 98, 40];
+console.log(Math.max(...randomNumbers)); // 800
+
+console.log('...');
+
+// Faça uma lista com as suas frutas favoritas
+const specialFruit = ['abacaxi', 'goiaba', 'melancia'];
+
+// Faça uma lista de complementos que você gostaria de adicionar
+const additionalItens = ['hamburguer', 'pizza', 'bolo'];
+
+const fruitSalad = (fruit, additional) => {
+  const allTogether = [...fruit, ...additional];
+  return allTogether;
+};
+
+console.log(fruitSalad(specialFruit, additionalItens));
+
+console.log('...');
+
+const countParams = (...args) => {
+  console.log('parâmetros:', args);
+  return `Você passou ${args.length} parâmetros para a função.`;
+};
+
+console.log(countParams(0, 1, 2)); // Você passou 3 parâmetros para a função.
+console.log(countParams('string', null, [1, 2, 3], {})); // Você passou 4 parâmetros para a função.
