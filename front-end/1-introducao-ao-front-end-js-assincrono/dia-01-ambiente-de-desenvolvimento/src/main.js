b3ee5f28983d320e077d867f75c7b3a660186ae4
thamyris <thamyriscabral@hotmail.com>
2023-02-08 11:52:44 -0300

    adicinada a biblioteca clipboard-copy e adicionado um evento de click ao h2

--- front-end/1-introducao-ao-front-end-js-assincrono/dia-01-ambiente-de-desenvolvimento/src/main.js
+++ front-end/1-introducao-ao-front-end-js-assincrono/dia-01-ambiente-de-desenvolvimento/src/main.js
@@ -1,12 +1,20 @@
 import { nanoid } from 'nanoid';
 import './main.css';
+import copy from 'clipboard-copy';
 
 const btn = document.querySelector('button');
 const h2 = document.querySelector('h2');
 
-btn.addEventListener('click', () => {
+const createSenha = () => {
   const gerador = nanoid();
   h2.innerHTML = gerador;
+};
+
+btn.addEventListener('click', createSenha);
+
+h2.addEventListener('click', (e) => {
+  copy(e.target.innerHTML);
+  alert('texto copiado');
 });
 
 // import { nanoid } from 'nanoid';
