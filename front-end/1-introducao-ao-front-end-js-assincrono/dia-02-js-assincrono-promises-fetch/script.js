c306a6167c1c7697adf3d33536b4bc1b5900e330
thamyris <thamyriscabral@hotmail.com>
2023-02-09 09:27:28 -0300

    exemplos de promisses e Js assincrono

---
+++ front-end/1-introducao-ao-front-end-js-assincrono/dia-02-js-assincrono-promises-fetch/script.js
@@ -0,0 +1,54 @@
+console.log('Log 1');
+
+setTimeout(() => console.log('Async log'), 1000);
+
+console.log('Log 2');
+console.log('Log 3');
+
+console.log('......');
+
+// função que gera um número aleatório
+const generateRandomNumber = () => Math.round(Math.random() * 10);
+
+// promise resolvida retornando o número aleatório
+const resolvedPromise = () =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const randomNumber = generateRandomNumber();
+      resolve(randomNumber);
+    }, 1000);
+  });
+
+resolvedPromise().then((response) => console.log(response));
+
+console.log('....');
+
+const rejectedPromise = () =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const randomNumber = generateRandomNumber();
+      reject(new Error(`O numero ${randomNumber} é um erro.`));
+    }, 1000);
+  });
+
+rejectedPromise().catch((error) => {
+  console.log(error.message);
+});
+
+console.log('......');
+
+const randomNumberPar = () =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const randomNumber = generateRandomNumber();
+      if (randomNumber % 2 === 0) {
+        resolve(randomNumber);
+      } else {
+        reject(new Error(`O numero ${randomNumber} não é par`));
+      }
+    }, 1000);
+  });
+
+randomNumberPar()
+  .then((response) => console.log(`${response} é par`))
+  .catch((error) => console.log(`${error.message}`));
