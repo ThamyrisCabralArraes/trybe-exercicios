ef7d817ef6187898fcdc70681b9eea3913b78001
thamyris <thamyriscabral@hotmail.com>
2023-02-09 18:57:10 -0300

    outro exercicio feito

---
+++ front-end/1-introducao-ao-front-end-js-assincrono/dia-02-js-assincrono-promises-fetch/exercicio-promises-fetch/main.js
@@ -0,0 +1,34 @@
+import './style.css';
+
+const imagemHeroi = document.getElementById('imagem-heroi');
+
+const botaoRandom = document.getElementById('botao-random');
+
+const randomNumber = (n) => {
+  return Math.floor(Math.random() * n);
+};
+
+const erro = (erro) => {
+  imagemHeroi.innerHTML = '';
+  const p = document.createElement('p');
+  p.textContent = 'Não achamos voce ainda. Clique Novamente';
+  imagemHeroi.appendChild(p);
+};
+
+botaoRandom.addEventListener('click', () => {
+  const random = randomNumber(500);
+  console.log(random);
+  fetch(`https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/id/${random}.json`)
+    .then((response) => response.json())
+    .then((data) => {
+      escolherImagemHeroi(data.images.md);
+    })
+    .catch((error) => erro(error.message));
+});
+
+export function escolherImagemHeroi(imageId) {
+  imagemHeroi.innerHTML = '';
+  const img = document.createElement('img');
+  img.src = imageId;
+  imagemHeroi.appendChild(img);
+}
