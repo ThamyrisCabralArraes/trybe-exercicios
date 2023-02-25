d1085b90f38bf385a8525a2bb16516f6df72af3c
thamyris <thamyriscabral@hotmail.com>
2023-02-25 11:28:55 -0300

    feat: criando 1 component About

---
+++ front-end/2-introducao-ao-react/exemplo1/src/About.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+  render() {
+    return (
+      <>
+        <h1>Thamyris</h1>
+        <p>Estudante de FullStack</p>
+        <ul>
+          <li>JS</li>
+          <li>JS Assíncrono</li>
+          <li>React</li>
+          <li>Tests</li>
+          <li>Iniciando TypeScript</li>
+        </ul>
+      </>
+    );
+  }
+}
+
+export default About;
--- front-end/2-introducao-ao-react/exemplo1/src/App.jsx
+++ front-end/2-introducao-ao-react/exemplo1/src/App.jsx
@@ -1,12 +1,17 @@
+import { Component } from 'react';
+import About from './About';
 import './App.css';
 import Hello from './Hello';
 
-function App() {
-  return (
-    <div className='App'>
-      <Hello />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Hello />
+        <About />
+      </div>
+    );
+  }
 }
 
 export default App;
