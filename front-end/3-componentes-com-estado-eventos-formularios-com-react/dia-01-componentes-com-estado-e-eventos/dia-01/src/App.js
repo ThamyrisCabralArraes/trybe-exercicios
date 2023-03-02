195f1f2845033d2cb14aef84389f8a330cfd566a
thamyris <thamyriscabral@hotmail.com>
2023-03-02 10:34:17 -0300

    feat: criado um evendo para mudar a cor do botao quando o numero for par

--- front-end/3-componentes-com-estado-eventos-formularios-com-react/dia-01-componentes-com-estado-e-eventos/dia-01/src/App.js
+++ front-end/3-componentes-com-estado-eventos-formularios-com-react/dia-01-componentes-com-estado-e-eventos/dia-01/src/App.js
@@ -4,24 +4,59 @@ import { Component } from 'react';
 
 
 class App extends Component {
-  handleBotao1 = () => {
-    console.log('clicou botao 1')
+  state = {
+    contagemClick1: 0,
+    contagemClick2: 0,
+    contagemClick3: 0,
   }
 
+  
+  handleBotao1 = () => {
+    this.setState((contagemAnterior, _props) => (
+      {contagemClick1: contagemAnterior.contagemClick1 + 1,
+      }),
+      )
+      }
+      
+  changeColor = (color) => color % 2 === 0 && color !== 0 ? 'pink' : 'white'
+  
   handleBotao2 = () => {
-    console.log('clicou botao 2')
+    this.setState((estadoAnterior, _) => (
+      {contagemClick2: estadoAnterior.contagemClick2 + 1}
+    ))
+    
   }
 
   handleBotao3 = () => {
-    console.log('clicou botao 3')
+    this.setState((esatdoAnterior, _) => (
+      {contagemClick3: esatdoAnterior.contagemClick3 + 1}
+    ))
   }
 
   render() {
+    const {contagemClick1} = this.state
+    const {contagemClick2} = this.state
+    const {contagemClick3} = this.state
   return (
     <div className="App">
-    <button onClick={this.handleBotao1}>botao1</button>
-    <button onClick={this.handleBotao2}>botao2</button>
-    <button onClick={this.handleBotao3}>botao3</button>
+    <button 
+    onClick={this.handleBotao1} 
+    style={{ background: this.changeColor(contagemClick1)}}
+    >
+      contagem click {contagemClick1}
+    </button>
+    <button 
+    onClick={this.handleBotao2} 
+    style={{ background: this.changeColor(contagemClick2)}}
+    >
+      contagem click {contagemClick2}
+    </button>
+    <button 
+    onClick={this.handleBotao3} 
+    style={{ background: this.changeColor(contagemClick3)}}
+    >
+      contagem click {contagemClick3}
+    </button>
     </div>
   );
   }
