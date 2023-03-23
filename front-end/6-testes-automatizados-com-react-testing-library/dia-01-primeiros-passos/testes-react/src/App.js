2d79d7f30b3ad0dd6be87810e5410e1683c45910
thamyris <thamyriscabral@hotmail.com>
2023-03-23 15:56:18 -0300

    feat: adicionado testes ai validEmail

--- front-end/6-testes-automatizados-com-react-testing-library/dia-01-primeiros-passos/testes-react/src/App.js
+++ front-end/6-testes-automatizados-com-react-testing-library/dia-01-primeiros-passos/testes-react/src/App.js
@@ -1,30 +1,55 @@
 // App.js
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import ValidEmail from './ValidEmail';
 
-function App() {
-  return (
-    <div className='App'>
-      <label htmlFor='id-email'>
-        Email
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      saveEmail: '',
+    };
+  }
+
+  changeEmail(value) {
+    this.setState({ email: value });
+  }
+
+  changeSaveEmail(value) {
+    this.setState({ saveEmail: value, email: '' });
+  }
+
+  render() {
+    const { email, saveEmail } = this.state;
+    return (
+      <div className='App'>
+        <label htmlFor='id-email'>
+          Email
+          <input
+            id='id-email'
+            value={email}
+            data-testid='id-email'
+            type='email'
+            onChange={(e) => this.changeEmail(e.target.value)}
+          />
+        </label>
         <input
-          id='id-email'
-          type='email'
+          id='btn-enviar'
+          type='button'
+          data-testid='id-send'
+          value='Enviar'
+          onClick={() => this.changeSaveEmail(email)}
         />
-      </label>
-      <input
-        id='btn-send'
-        type='button'
-        value='Enviar'
-        data-testid='id-send'
-      />
-      <input
-        id='btn-back'
-        type='button'
-        value='Voltar'
-      />
-    </div>
-  );
+        <input
+          id='btn-id'
+          type='button'
+          value='Voltar'
+        />
+        <ValidEmail email={saveEmail} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- front-end/6-testes-automatizados-com-react-testing-library/dia-01-primeiros-passos/testes-react/src/App.test.js
+++ front-end/6-testes-automatizados-com-react-testing-library/dia-01-primeiros-passos/testes-react/src/App.test.js
@@ -1,27 +1,29 @@
 import { render, screen } from '@testing-library/react';
+
 import App from './App';
+describe('testando o evendo do input e botao', () => {
+  it('testando input', () => {
+    render(<App />);
+    const inputEmail = screen.getByLabelText(/email/i);
 
-test('testando input', () => {
-  render(<App />);
-  const inputEmail = screen.getByLabelText(/email/i);
+    expect(inputEmail).toBeInTheDocument();
+    // expect(inputEmail.type).tobe('email');
+    expect(inputEmail).toHaveProperty('type', 'email');
+  });
 
-  expect(inputEmail).toBeInTheDocument();
-  // expect(inputEmail.type).tobe('email');
-  expect(inputEmail).toHaveProperty('type', 'email');
-});
+  it('testando input sem texto', () => {
+    render(<App />);
+    const buttons = screen.getAllByRole('button');
 
-test('testando input sem texto', () => {
-  render(<App />);
-  const buttons = screen.getAllByRole('button');
+    expect(buttons).toHaveLength(2);
+  });
 
-  expect(buttons).toHaveLength(2);
-});
+  it('testando botao enviar', () => {
+    render(<App />);
+    const bntEnviar = screen.getByTestId(/id-send/i);
 
-test('testando botao enviar', () => {
-  render(<App />);
-  const bntEnviar = screen.getByTestId(/id-send/i);
-
-  expect(bntEnviar).toBeInTheDocument();
-  expect(bntEnviar).toHaveValue('Enviar');
-  expect(bntEnviar).toHaveProperty('type', 'button');
+    expect(bntEnviar).toBeInTheDocument();
+    expect(bntEnviar).toHaveValue('Enviar');
+    expect(bntEnviar).toHaveProperty('type', 'button');
+  });
 });
