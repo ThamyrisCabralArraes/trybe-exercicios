350bb754048ad85c863c9db8a1a5c15978f51888
thamyris <thamyriscabral@hotmail.com>
2023-04-03 15:12:43 -0300

    exercicio 1 realizado

--- front-end/7-redux/dia-01-introducao-ao-redux/exemplos-redux/main.js
+++ front-end/7-redux/dia-01-introducao-ao-redux/exemplos-redux/main.js
@@ -1,6 +1,7 @@
 import { legacy_createStore as createStore } from 'redux';
 import { composeWithDevTools } from '@redux-devtools/extension';
 
+// criando o esatdo inicial
 const INICIAL_STATE = { count: 0 };
 
 const reducer = (state = INICIAL_STATE, action) => {
@@ -10,15 +11,19 @@ const reducer = (state = INICIAL_STATE, action) => {
   return state;
 };
 
+// criando a store do redux dev tools
 const store = createStore(reducer, composeWithDevTools());
 
+// criando a action
 const action = { type: 'INCREMENT_COUNTER' };
 
+// disparando a action
 const incrementarBtn = document.querySelector('#incrementar');
 incrementarBtn.addEventListener('click', () => {
   store.dispatch(action);
 });
 
+// lendo as alterações do estado
 store.subscribe(() => {
   const count = document.querySelector('h2');
   const globalState = store.getState();
