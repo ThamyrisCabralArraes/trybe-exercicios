e9e3d3d414e55a7861ee7a81339995cc4dfcb720
thamyris <thamyriscabral@hotmail.com>
2023-04-04 10:45:30 -0300

    feat: criada pasta reducer e adicionada uma funçao de reducer

--- front-end/7-redux/dia-02-usando-redux-react/exemplos-redux-react/src/App.js
+++ front-end/7-redux/dia-02-usando-redux-react/exemplos-redux-react/src/App.js
@@ -1,7 +1,23 @@
-import './App.css';
+import React from 'react';
 
-function App() {
-  return <div className='App'></div>;
+import { connect } from 'react-redux';
+
+class App extends React.Component {
+  render() {
+    const { countState } = this.props;
+    return (
+      <div>
+        <h1>Contador</h1>
+        <h2>{countState}</h2>
+        <button>Incrementa 1</button>
+        <button>Incrementa 5</button>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  countState: state.count,
+});
+
+export default connect(mapStateToProps)(App);
