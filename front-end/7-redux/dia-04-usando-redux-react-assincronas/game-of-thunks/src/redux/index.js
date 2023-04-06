dfc8bf2430ecb710ec5d2aff6276f48316a7bca5
thamyris <thamyriscabral@hotmail.com>
2023-04-06 15:33:12 -0300

    feat: feito pastas redux, actions, reducers

---
+++ front-end/7-redux/dia-04-usando-redux-react-assincronas/game-of-thunks/src/redux/index.js
@@ -0,0 +1,5 @@
+import { legacy_createStore as createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from '@redux-devtools/extension';
+import thunk from 'redux-thunk';
+
+const store = createStore(applyMiddleware(thunk))
