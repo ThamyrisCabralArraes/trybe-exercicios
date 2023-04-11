17562775dbe3a16974220489e5d8895e4cf9d6f0
thamyris <thamyriscabral@hotmail.com>
2023-04-11 09:36:25 -0300

    feat: adicionado codigo ao caracteriosticas

--- front-end/7-redux/dia-04-usando-redux-react-assincronas/game-of-thunks/src/components/Caractesristicas.js
+++ front-end/7-redux/dia-04-usando-redux-react-assincronas/game-of-thunks/src/components/Caractesristicas.js
@@ -1,14 +1,41 @@
 import React from 'react';
+import { connect } from 'react-redux';
 // import PropTypes from 'prop-types'
 
 export const Caractesristicas = (props) => {
-  return (
-    <div>
-      <h1>Caracteristicas</h1>
-    </div>
-  );
+  const { character, loading } = this.props;
+  if (!loading && character) {
+    return (
+      <ul>
+        <li>Name: {character.name}</li>
+        <li>Born: {character.born}</li>
+        <li>
+          Titles:
+          <ol>
+            {character.titles.map((title, index) => (
+              <li key={`${title}-${index}`}>{title}</li>
+            ))}
+          </ol>
+        </li>
+        <li>
+          Aliases:
+          <ol>
+            {character.aliases.map((alias, index) => (
+              <li key={`${alias}-${index}`}>{alias}</li>
+            ))}
+          </ol>
+        </li>
+      </ul>
+    );
+  }
 };
 
 Caractesristicas.propTypes = {};
 
-export default Caractesristicas;
+const mapStateToProps = (state) => ({
+  character: state.character,
+  loading: state.isLoading,
+  error: state.error,
+});
+
+export default connect(mapStateToProps)(Caractesristicas);
